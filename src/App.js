64d49d18bf3b8ab503cddba58ed1e680486b0351
Rashmi <rashmi@gmail.com>
2020-07-11 13:05:15 +0530

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import { Card, Container, Button } from 'react-bootstrap';
 
 function App() {
+  const [date,setDate] = useState(null);
+  const[day,setday] =useState(null);
+  const[month,setmonth] =useState(null);
+  const[year,setyear] =useState(null);
+  useEffect(()=>{
+    if(date){
+      const val = date.date.split('-');
+      setday(val[0]);
+      setmonth(val[1]);
+      setyear(val[2]);
+      console.log("val=="+val)
+        }
+  },[day,month,year,date])
+
+  console.log(date);
+ const  getDateInfo =() =>{
+  fetch('https://jsonmock.hackerrank.com/datetime')
+  .then(response => response.json())
+    .then(data => setDate(data));
+ }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Container className="text-center">
+<h1 className="text-center">Date API</h1>
+<br/>
+<Button className="text-center" variant="primary" onClick={getDateInfo}>Display Date</Button>
+<br/><br/>
+  <Card>
+    <Card.Body>
+      <Card.Text>Day: {day} </Card.Text>
+      <Card.Text>Month: {month} </Card.Text>
+      <Card.Text>Year: {year} </Card.Text>
+    </Card.Body>
+  </Card>
+  </Container>
   );
 }
 
